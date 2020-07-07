36e92e8ec870b8226b51362c9b9d72ee69ad0383
M Rub Shuvo <MRUBS@users.noreply.github.com>
2020-07-07 12:22:09 +0600

    update express functions

--- app.js
+++ app.js
@@ -15,3 +15,11 @@ app.get('/about', (req, res) => {
 	// res.send('<p>this is the about page</p>');
 	res.sendFile('./views/about.html', { root: __dirname });
 });
+
+app.get('/about-us', (req, res) => {
+	res.redirect('/about');
+});
+
+app.use((req, res) => {
+	res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
