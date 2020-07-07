a8015c56cb82c0ecbeda0135ddb24068f826b6b4
M Rub Shuvo <MRUBS@users.noreply.github.com>
2020-07-07 17:18:27 +0600

    middleware & morgan

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for request
 app.listen(3000);
 
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
-	// res.send('<p>this is the home page</p>');
-	res.sendFile('./views/index.html', { root: __dirname });
+	const blogs = [
+		{ title: "title one", snippet: 'blog snippet one' },
+		{ title: "title two", snippet: 'blog snippet two' },
+		{ title: "title three", snippet: 'blog snippet three' }
+	];
+	res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
-	// res.send('<p>this is the about page</p>');
-	res.sendFile('./views/about.html', { root: __dirname });
+	res.render('about', { title: 'About' });
 });
 
-app.get('/about-us', (req, res) => {
-	res.redirect('/about');
-});
+app.get('/blogs/create', (req, res) => {
+	res.render('create', { title: 'Create a new blog' });
+})
 
 app.use((req, res) => {
-	res.status(404).sendFile('./views/404.html', { root: __dirname });
+	res.status(404).render('404', {title: '404 not found'});
 });
