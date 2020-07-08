c357c57d8ce8955af17982a68e54febdeb293cb7
M Rub Shuvo <MRUBS@users.noreply.github.com>
2020-07-08 17:35:47 +0600

    disable blogs sandbox routes

--- app.js
+++ app.js
@@ -1,24 +1,69 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://nodes:QU4HMoO6KBDpddkW@nodes.gpano.mongodb.net/nodes?retryWrites=true&w=majority';
+mongoose
+	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => app.listen(3000))
+	.catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
-
-// listen for request
-app.listen(3000);
 
 // middleware & static files
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+// mongoose and mongo sandbox routes
+// app.get('/add-blog', (req, res) => {
+// 	const blog = new Blog({
+// 		title: 'new blog 2',
+// 		snippet: 'about my new blog',
+// 		body:
+// 			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam consequat nulla convallis ligula placerat, ut suscipit ex molestie. Donec congue ut metus at eleifend.'
+// 	});
+
+// 	blog
+// 		.save()
+// 		.then((result) => {
+// 			res.send(result);
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 		});
+// });
+
+// app.get('/all-blogs', (req, res) => {
+// 	Blog.find()
+// 		.then((result) => {
+// 			res.send(result);
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 		});
+// });
+
+// app.get('/single-blog', (req, res) => {
+// 	Blog.findById('5f05a8d755e7081168a72188')
+// 		.then((result) => {
+// 			res.send(result);
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 		});
+// });
+
 app.get('/', (req, res) => {
 	const blogs = [
-		{ title: "title one", snippet: 'blog snippet one' },
-		{ title: "title two", snippet: 'blog snippet two' },
-		{ title: "title three", snippet: 'blog snippet three' }
+		{ title: 'title one', snippet: 'blog snippet one' },
+		{ title: 'title two', snippet: 'blog snippet two' },
+		{ title: 'title three', snippet: 'blog snippet three' }
 	];
 	res.render('index', { title: 'Home', blogs });
 });
@@ -29,8 +74,8 @@ app.get('/about', (req, res) => {
 
 app.get('/blogs/create', (req, res) => {
 	res.render('create', { title: 'Create a new blog' });
-})
+});
 
 app.use((req, res) => {
-	res.status(404).render('404', {title: '404 not found'});
+	res.status(404).render('404', { title: '404 not found' });
 });
